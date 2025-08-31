0e89f13f42bcbef6256a7a18cad8f26512307f90
Sathya <neelasathya06@gmail.com>
2025-08-31 18:36:29 -0400

    map intro

--- map.go
+++ map.go
@@ -4,10 +4,19 @@ import "fmt"
 
 func main() {
 
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	//var colors map[string]string
+
+	colors := make(map[int]string)
+
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#4bf745",
+	//}
+
+	colors[1] = "#FF0000"
+	colors[2] = "#0000FF"
+
+	delete(colors, 1)
 
 	fmt.Println(colors)
 }
